02c597f018c1bf6cdd244a2c30817617d3730ee9
Roberto Dailey <dailey.roberto@gmail.com>
2015-02-02 23:07:10 -0600

    added basic methodology for complex variable

--- src/Complex.java
+++ src/Complex.java
@@ -2,4 +2,82 @@
  * Created by Roberto on 2/2/2015.
  */
 public class Complex {
+
+    private double real;
+    private double imaginary;
+
+
+    public Complex(double real, double imaginary) {
+        this.imaginary = imaginary;
+        this.real = real;
+
+    }
+
+    public Complex(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    public static Complex addComplex(Complex a, Complex b)
+    {
+        double r;
+        double i;
+        r = a.getReal() + b.getReal();
+        i = a.getImaginary() + b.getImaginary();
+        return new Complex(r,i);
+
+    }
+
+    public static Complex subtractComplex(Complex a, Complex b)
+    {
+        double r;
+        double i;
+        r = a.getReal() + b.getReal();
+        i = a.getImaginary() + b.getImaginary();
+        return new Complex(r,i);
+    }
+
+    public Complex multiplyComplex(Complex a, Complex b)
+    {
+        double r;
+        double i;
+        r = a.getReal() + b.getReal();
+        i = a.getImaginary() + b.getImaginary();
+        return new Complex(r,i);
+    }
+
+    public Complex devideComplex(Complex a, Complex b)
+    {
+        double r;
+        double i;
+        r = a.getReal() + b.getReal();
+        i = a.getImaginary() + b.getImaginary();
+        return new Complex(r,i);
+    }
+
+    public Complex sinComplex(Complex a)
+    {
+        return new Complex(Math.sin(real) * Math.cosh(imaginary), Math.cos(real) * Math.sinh(imaginary));
+    }
+
+    public Complex cosComplex(Complex a)
+    {
+        return new Complex(Math.cos(real) * Math.cosh(imaginary), -Math.sin(real) * Math.sinh(imaginary));
+    }
+
+    public Complex tanComplex(Complex a)
+    {
+        return devideComplex(sinComplex(a),cosComplex(a));
+
+    }
+
+
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
 }
